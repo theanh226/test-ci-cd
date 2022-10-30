9af0725eab7d81ae6b9480d9f4b05e968008a6ff
theanh226 <theanh0226@gmail.com>
2022-10-30 23:34:29 +0100

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello theanhtran ls a test
+          remove
         </a>
       </header>
     </div>
